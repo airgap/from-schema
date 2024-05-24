f80743f4ec6981907b6d4f36c18bf8505000505f
Nicole Muzzin <9166649+airgap@users.noreply.github.com>
2024-05-24 00:41:48 -0400

    Docs

--- src/StringSchema.ts
+++ src/StringSchema.ts
@@ -4,6 +4,7 @@ export type StringSchema = SchemaBase & {
 	readonly type: 'string';
 	readonly minLength?: number;
 	readonly maxLength?: number;
+	// Regular expression, e.g. "^[A-Z][a-z]{1,9}$"
 	readonly pattern?: string;
 	readonly format?: string;
 };
