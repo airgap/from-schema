c61f980bf404efb698d4f667573b19951f9ca5ab
airgap <nicole@muzz.in>
2024-05-24 00:45:45 -0400

    Untested Union patch

--- src/UnionSchema.ts
+++ src/UnionSchema.ts
@@ -1,17 +1,5 @@
 import { SchemaBase } from './SchemaBase';
-import { Primitive } from './Primitive';
-import { ObjectSchema } from './ObjectSchema';
-import { ArraySchema } from './ArraySchema';
-import { StringSchema } from './StringSchema';
-import { BooleanSchema } from './BooleanSchema';
-import { NumberSchema } from './NumberSchema';
+import { SchemaOrPrimitive } from './SchemaOrPrimitive';
 export type UnionSchema = SchemaBase & {
-	readonly union: readonly (
-		| Primitive
-		| ObjectSchema
-		| ArraySchema
-		| StringSchema
-		| BooleanSchema
-		| NumberSchema
-	)[];
+	readonly union: readonly SchemaOrPrimitive[];
 };
--- src/UnionSchemaOf.ts
+++ src/UnionSchemaOf.ts
@@ -1,5 +1,6 @@
 import { SchemaBase } from './SchemaBase';
+import { SchemaOrPrimitive } from './SchemaOrPrimitive';
 
-export type UnionSchemaOf<K> = SchemaBase & {
+export type UnionSchemaOf<K extends SchemaOrPrimitive> = SchemaBase & {
 	readonly union: readonly K[];
 };
