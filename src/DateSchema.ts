769a8013e85bba8520912c2f0422830eec67be9c
Nicole Muzzin <nicole@muzz.in>
2024-07-05 08:40:17 -0400

    minimum/maximum is now opt on dates

--- src/DateSchema.ts
+++ src/DateSchema.ts
@@ -1,12 +1,12 @@
 export type JsonDate = {
 	readonly type: 'string';
 	readonly format: 'date-time';
-	readonly minimum: string;
-	readonly maximum: string;
+	readonly minimum?: string;
+	readonly maximum?: string;
 };
 export type BsonDate = {
 	readonly bsonType: 'date';
-	readonly minimum: Date;
-	readonly maximum: Date;
+	readonly minimum?: Date;
+	readonly maximum?: Date;
 };
 export type DateSchema = JsonDate | BsonDate;
