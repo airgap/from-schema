e9619a0366707a3d2d2ba1f434e9410785c30aa9
Nicole Muzzin <nicole@muzz.in>
2024-07-14 02:14:02 -0400

    sometimes say never

--- src/OnlyOptional.ts
+++ src/OnlyOptional.ts
@@ -5,4 +5,4 @@ import { RequiredPropOf } from './RequiredPropOf';
 export type OnlyOptional<T extends ObjectSchema> =
 	T extends ObjectSchemaWithRequired
 		? Partial<Omit<FromPropertySchemas<T>, RequiredPropOf<T>>>
-		: Partial<T['properties']>;
+		: Record<string, never>;
