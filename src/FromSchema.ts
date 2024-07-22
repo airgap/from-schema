8c1859d8db91e2d3e0ffd830e3a69d103fd55121
Nicole Muzzin <nicole@muzz.in>
2024-07-22 00:17:43 -0400

    Add mongodb optionally for ObjectId

--- src/FromSchema.ts
+++ src/FromSchema.ts
@@ -10,23 +10,26 @@ import { FromMapSchema, MapSchema, MapSchemaOf } from './MapSchema';
 import { UnionSchemaOf } from './UnionSchema';
 import { DateSchema } from './DateSchema';
 import { ObjectIdSchema } from './ObjectIdSchema';
+import { ObjectId } from 'mongodb';
 
 export type FromSchema<T> = T extends DateSchema
 	? Date | string
-	: T extends StringSchema | ObjectIdSchema
-		? string
-		: T extends NumberSchema
-			? number
-			: T extends BooleanSchema
-				? boolean
-				: T extends UnionSchemaOf<infer P>
-					? PrimitiveOrFromSchema<P>
-					: T extends EnumSchemaOf<infer P>
+	: T extends ObjectIdSchema
+		? ObjectId | string
+		: T extends StringSchema
+			? string
+			: T extends NumberSchema
+				? number
+				: T extends BooleanSchema
+					? boolean
+					: T extends UnionSchemaOf<infer P>
 						? PrimitiveOrFromSchema<P>
-						: T extends ObjectSchema
-							? FromObjectSchema<T>
-							: T extends MapSchema
-								? FromMapSchema<T>
-								: T extends ArraySchema
-									? FromSchema<T['items']>[]
-									: never;
+						: T extends EnumSchemaOf<infer P>
+							? PrimitiveOrFromSchema<P>
+							: T extends ObjectSchema
+								? FromObjectSchema<T>
+								: T extends MapSchema
+									? FromMapSchema<T>
+									: T extends ArraySchema
+										? FromSchema<T['items']>[]
+										: never;
