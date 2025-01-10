ad53945586207feba371fee31ecf5974edf4b6e0
Nicole Muzzin <nicole@muzz.in>
2025-01-10 05:07:33 -0500

    smol patch

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,5 +1,5 @@
 export type Validator = {
 	validate: (value: unknown) => string[];
 	validateOrThrow: (value: unknown) => void;
-	isValid: (value: unknown) => true | string;
+	isValid: ((value: unknown) => true | string) | ((value: unknown) => true);
 };
