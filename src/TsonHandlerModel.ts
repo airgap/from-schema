447298cc3c5557abf625e400e7b6835738a466b2
Nicole Muzzin <nicole@muzz.in>
2025-01-13 07:23:45 -0500

    Add generated and unique

--- src/TsonHandlerModel.ts
+++ src/TsonHandlerModel.ts
@@ -3,6 +3,7 @@ import { TsonSchema } from './tson';
 export type TsonHandlerBase = {
 	request?: TsonSchema;
 	response?: TsonSchema;
+	authenticated?: boolean;
 };
 
 export type StreamConfig = {
@@ -11,14 +12,14 @@ export type StreamConfig = {
 };
 
 // export type HandlerModel = HandlerModelBase & (AuthenticatedHandlerModel | AnonymouseHandlerModel) & (StreamHandlerModel | HttpHandlerModel)
-export type TsonHttpHandlerModel = TsonHandlerBase & Readonly<{
-	authenticated?: boolean;
-	stream?: false;
-}>;
+export type TsonHttpHandlerModel = TsonHandlerBase &
+	Readonly<{
+		stream?: false;
+	}>;
 
-export type TsonStreamHandlerModel = TsonHandlerBase & Readonly<{
-	authenticated?: boolean;
-	stream: true | StreamConfig;
-}>;
+export type TsonStreamHandlerModel = TsonHandlerBase &
+	Readonly<{
+		stream: true | StreamConfig;
+	}>;
 
 export type TsonHandlerModel = TsonHttpHandlerModel | TsonStreamHandlerModel;
--- src/generic/SchemaBase.ts
+++ src/generic/SchemaBase.ts
@@ -1,4 +1,11 @@
 export type SchemaBase = {
 	readonly description?: string;
 	readonly title?: string;
+	readonly unique?: boolean;
+	readonly generated?: {
+		readonly always?: boolean;
+		readonly as: string;
+		readonly from: string;
+		readonly stored?: boolean;
+	};
 };
