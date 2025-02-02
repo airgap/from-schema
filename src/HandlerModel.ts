d9c14ebc2980ac40798a7bad376835b105fa623c
Nicole Muzzin <nicole@muzz.in>
2025-02-02 02:57:25 -0500

    Add methods

--- src/HandlerModel.ts
+++ src/HandlerModel.ts
@@ -1,9 +1,39 @@
 import { JsonSchema } from './json';
 
 // export type HandlerModel = HandlerModelBase & (AuthenticatedHandlerModel | AnonymouseHandlerModel) & (StreamHandlerModel | HttpHandlerModel)
-export type HandlerModel = Readonly<{
-	request?: JsonSchema;
+type HandlerBase = {
 	response?: JsonSchema;
 	authenticated?: boolean;
-	stream?: boolean;
-}>;
+};
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'OPTIONS' | 'HEAD';
+type GetHandlerModel = HandlerBase & {
+	method: 'GET';
+};
+type PostHandlerModel = HandlerBase & {
+	method?: 'POST';
+	request?: JsonSchema;
+};
+type PutHandlerModel = HandlerBase & {
+	method: 'PUT';
+	request?: JsonSchema;
+};
+type DeleteHandlerModel = HandlerBase & {
+	method: 'DELETE';
+	request?: JsonSchema;
+};
+type PatchHandlerModel = HandlerBase & {
+	method: 'PATCH';
+	request?: JsonSchema;
+};
+type WebsocketHandlerModel = HandlerBase & {
+	stream: true;
+	request?: JsonSchema;
+};
+export type HandlerModel = Readonly<
+	| GetHandlerModel
+	| PostHandlerModel
+	| PutHandlerModel
+	| DeleteHandlerModel
+	| PatchHandlerModel
+	| WebsocketHandlerModel
+>;
