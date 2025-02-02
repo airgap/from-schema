920e18a550f81749ee9887271b2ff2de5a47ea8d
Nicole Muzzin <nicole@muzz.in>
2025-02-02 03:12:15 -0500

    TSON

--- src/TsonHandlerModel.ts
+++ src/TsonHandlerModel.ts
@@ -1,7 +1,6 @@
 import { TsonSchema } from './tson';
 
 export type TsonHandlerBase = {
-	request?: TsonSchema;
 	response?: TsonSchema;
 	authenticated?: boolean;
 };
@@ -11,15 +10,51 @@ export type StreamConfig = {
 	tweakResponse?: TsonSchema;
 };
 
-// export type HandlerModel = HandlerModelBase & (AuthenticatedHandlerModel | AnonymouseHandlerModel) & (StreamHandlerModel | HttpHandlerModel)
-export type TsonHttpHandlerModel = TsonHandlerBase &
-	Readonly<{
-		stream?: false;
-	}>;
+type GetHandlerModel = TsonHandlerBase & {
+	method: 'GET';
+};
 
-export type TsonStreamHandlerModel = TsonHandlerBase &
-	Readonly<{
-		stream: true | StreamConfig;
-	}>;
+type PostHandlerModel = TsonHandlerBase & {
+	method: 'POST';
+	request?: TsonSchema;
+};
+
+type PutHandlerModel = TsonHandlerBase & {
+	method: 'PUT';
+	request?: TsonSchema;
+};
+
+type DeleteHandlerModel = TsonHandlerBase & {
+	method: 'DELETE';
+	request?: TsonSchema;
+};
+
+type PatchHandlerModel = TsonHandlerBase & {
+	method: 'PATCH';
+	request?: TsonSchema;
+};
+
+type OptionsHandlerModel = TsonHandlerBase & {
+	method: 'OPTIONS';
+};
+
+type HeadHandlerModel = TsonHandlerBase & {
+	method: 'HEAD';
+};
+
+// export type HandlerModel = HandlerModelBase & (AuthenticatedHandlerModel | AnonymouseHandlerModel) & (StreamHandlerModel | HttpHandlerModel)
+export type TsonHttpHandlerModel = Readonly<
+	| GetHandlerModel
+	| PostHandlerModel
+	| PutHandlerModel
+	| DeleteHandlerModel
+	| PatchHandlerModel
+	| OptionsHandlerModel
+	| HeadHandlerModel
+>;
+
+export type TsonStreamHandlerModel = Readonly<
+	TsonHandlerBase & { stream: true | StreamConfig }
+>;
 
 export type TsonHandlerModel = TsonHttpHandlerModel | TsonStreamHandlerModel;
