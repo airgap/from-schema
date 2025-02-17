39c9f9e1db11ac83d94be1e0881958e8bb56672d
Nicole Muzzin <nicole@muzz.in>
2025-02-16 21:12:42 -0500

    bigint

--- src/postgres/columns/numeric/NumberBase.ts
+++ src/postgres/columns/numeric/NumberBase.ts
@@ -1,11 +1,11 @@
 import { PostgresColumnBase } from '../../PostgresColumnBase';
 
-export type NumberBase = PostgresColumnBase & {
-	readonly minimum?: number;
-	readonly maximum?: number;
-	readonly exclusiveMinimum?: number;
-	readonly exclusiveMaximum?: number;
-	readonly multipleOf?: number;
-	readonly examples?: number[];
-	readonly default?: number | 'nextval';
+export type NumberBase<T extends number | bigint = number> = PostgresColumnBase & {
+	readonly minimum?: T;
+	readonly maximum?: T;
+	readonly exclusiveMinimum?: T;
+	readonly exclusiveMaximum?: T;
+	readonly multipleOf?: T;
+	readonly examples?: T[];
+	readonly default?: T | 'nextval';
 };
