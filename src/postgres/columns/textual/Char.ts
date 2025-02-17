b1dde95bd279b68776b66fc0767a6084754d008c
Nicole Muzzin <nicole@muzz.in>
2025-02-16 20:45:30 -0500

    Add checks to char

--- src/postgres/columns/textual/Char.ts
+++ src/postgres/columns/textual/Char.ts
@@ -4,4 +4,6 @@ export type CharColumnModel = PostgresColumnBase & {
 	readonly type: 'char' | 'character' | 'bpchar';
 	readonly length: number;
 	readonly pattern?: string;
+	readonly enum?: string[];
+	readonly checks?: string[];
 };
