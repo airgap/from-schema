dc13a58e3476fe73273f8b7f804a418dc32cbfa0
Nicole Muzzin <nicole@muzz.in>
2025-02-20 22:35:39 -0500

    Insertable

--- src/typeverters/postgresRecordToType.ts
+++ src/typeverters/postgresRecordToType.ts
@@ -3,10 +3,18 @@ import { PostgresColumnModel } from '../postgres/PostgresColumnModel';
 import { postgresColumnToType } from './postgresColumnToType';
 export const postgresRecordToType = <S extends PostgresRecordModel>(
 	s: S,
+	insertable: boolean,
 ): string => {
 	const properties = Object.entries(
 		s.properties as Record<string, PostgresColumnModel>,
 	);
 	const required = 'required' in s ? s.required : [];
-	return `{ ${properties.map(([k, v]) => `${k}${!required.includes(k) ? '?' : ''}: ${postgresColumnToType(v)}`).join('; ')} }`;
+	return `{ ${properties
+		.map(([k, v]) => {
+			const insertableAndPrefilled =
+				insertable && !('default' in v) && !('generated' in v);
+			const optional = insertableAndPrefilled || !required.includes(k);
+			return `${k}${optional ? '?' : ''}: ${postgresColumnToType(v)}`;
+		})
+		.join('; ')} }`;
 };
