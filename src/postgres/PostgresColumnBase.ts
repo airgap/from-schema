b990e8209ac8e2c3c2e95e9d46905131cf0ab3b9
Nicole Muzzin <nicole@muzz.in>
2025-07-12 20:58:12 -0400

    stuff idk

--- src/postgres/PostgresColumnBase.ts
+++ src/postgres/PostgresColumnBase.ts
@@ -4,6 +4,7 @@ export type PostgresColumnBase = SchemaBase & {
 	readonly unique?: boolean;
 	readonly primaryKey?: boolean;
 	readonly generated?: {
+		readonly always?: boolean;
 		readonly as: string;
 		readonly from?: string;
 		readonly stored?: boolean;
